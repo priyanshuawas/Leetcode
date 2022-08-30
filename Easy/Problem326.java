04fdfe15fd4069fa0c334ef244c0408d5d96a9fc
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-08-30 19:30:50 +0530

    Code

---
+++ Easy/Problem326.java
@@ -0,0 +1,16 @@
+package Easy;
+
+public class Problem326 {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n = n / 3;
+        }
+        return true;
+    }
+}
---
+++ Easy/Problem383.java
@@ -0,0 +1,18 @@
+package Easy;
+
+class Problem383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote == null) { return true; }
+        if (magazine == null) { return false; }
+        if (magazine.length() < ransomNote.length()) { return false; }
+        int[] count = new int[26];
+        for (char c: magazine.toCharArray()) {
+            count[c - 'a'] += 1;
+        }
+        for (char c: ransomNote.toCharArray()) {
+            if (count[c - 'a'] == 0) { return false; }
+            count[c - 'a'] -= 1;
+        }
+        return true;
+    }
+}
