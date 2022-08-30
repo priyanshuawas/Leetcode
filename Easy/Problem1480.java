476d909c1fc5f9776f76d98e65258394b84f955d
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-08-30 19:28:34 +0530

    Code

---
+++ Easy/Problem1480.java
@@ -0,0 +1,11 @@
+package Easy;
+
+class Problem1480 {
+    public int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            nums[i] += nums[i-1];
+        }
+        return nums;
+        
+    }
+}
