8bdc0a4fed1bfe99e77056ac619eafe54e1ae1d1
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-08-30 19:38:14 +0530

    Code

---
+++ Medium/Problem200.java
@@ -0,0 +1,33 @@
+package Medium;
+
+
+class Problem200{
+    public int numIslands(char[][] grid) {
+        
+        int m = grid.length, n = grid[0].length, count = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    void dfs(char[][] grid, int row, int col){
+        
+        if(row < 0 || col < 0 || row >= grid.length 
+           || col >= grid[0].length || grid[row][col] == '0') return;
+		   
+        grid[row][col] = '0';
+        
+        dfs(grid, row-1, col);
+        dfs(grid, row, col-1);
+        dfs(grid, row+1, col);
+        dfs(grid, row, col+1);
+    }
+}
+// Time Complexity :O(m*n)
+// Space Complexity : O(1)
