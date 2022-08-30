f09856807e6c3d4fe1be2eb46df513643a428540
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-08-30 19:34:28 +0530

    Code

---
+++ Hard/Problem363.java
@@ -0,0 +1,26 @@
+package Hard;
+
+class Problem363{
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int m = matrix.length, n = matrix[0].length;
+        int res = Integer.MIN_VALUE;
+        for (int r1 = 0; r1 < m; r1++) {
+            int[] presum = new int[n];
+            for (int r2 = r1; r2 < m; r2++) {
+                TreeSet<Integer> set = new TreeSet<Integer>();
+                set.add(0);
+                int val = 0;
+                for (int c = 0; c < n; c++) {
+                    presum[c] += matrix[r2][c];
+                    val += presum[c];
+                    Integer prev = set.ceiling(val - k);
+                    if (prev != null) {
+                        res = Math.max(res, val - prev);
+                    }
+                    set.add(val);
+                }
+            }
+        }
+        return res;
+    }
+}
