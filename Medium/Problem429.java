172bd7c543b025ff5572c574e18a115687d1d4fb
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-09-05 22:59:08 +0530

    Code

---
+++ Medium/Problem429.java
@@ -0,0 +1,29 @@
+package Medium;
+
+class Problem429 {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+
+        Queue<Node> qu = new LinkedList<>();
+
+        qu.offer(root);
+
+        while(!qu.isEmpty()) {
+            int size = qu.size();
+            List<Integer> nodesAtOneLevel = new ArrayList<>();
+            while(size-->0){
+                Node head = qu.poll();
+                nodesAtOneLevel.add(head.val);
+                for(Node child : head.children){
+                    qu.offer(child);
+                }
+            }
+            ans.add(nodesAtOneLevel);
+        }
+
+        return ans;
+    }
+}
