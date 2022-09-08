054aabf5df3505f49a870e85e22498c36bb079b1
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-09-08 21:39:59 +0530

    Binary Tree Inorder Traversal

---
+++ Easy/Problem94.java
@@ -0,0 +1,18 @@
+package Easy;
+class Problem94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            list.add(stack.peek().val);
+            node = stack.pop().right;
+        }
+        return list;
+    }
+}
