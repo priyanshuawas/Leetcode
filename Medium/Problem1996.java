a83878e3bfec58e8267eafe07252012fb69e2506
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-09-09 23:31:34 +0530

    The Number of Weak Characters in the Game

---
+++ Medium/Problem1996.java
@@ -0,0 +1,18 @@
+package Medium;
+
+class Problem1996 {
+    public int numberOfWeakCharacters(int[][] properties) {
+        
+        Arrays.sort(properties,(a,b)->(a[0] == b[0])?b[1]-a[1]:a[0]-b[0]);
+        int n = properties.length;
+        int max = properties[n-1][1];
+        int ans = 0;
+        for(int i = n-2;i >= 0; i--){
+            if(properties[i][1]<max) ans++;
+            
+            else
+            max = properties[i][1];  
+        }
+        return ans; 
+    }
+}
