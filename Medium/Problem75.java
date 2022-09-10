c5402bdd5e26b63ab4be2da357949eb52d201baa
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-09-10 17:37:21 +0530

    Changes

--- Medium/Problem75.java
+++ Medium/Problem75.java
@@ -2,13 +2,13 @@ package Medium;
 class Problem75 {
     public void sortColors(int[] nums) {
     
-        int red=0, yellow=0,white=0;
+        int red=0, y =0,white=0;
         for(int i =0;i<nums.length;i++){
             if(nums[i]==0)
                 red++;
             if(nums[i]==1)
                 white++;
-            yellow=nums.length-(white+red);
+            y=nums.length-(white+red);
         }
         for(int i=0;i<red;i++){
             nums[i]=0;
