a26644945a7b60c72b63cb13a19af213d93ed884
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-09-14 22:45:51 +0530

    Pseudo-Palindromic Paths in a Binary Tree

---
+++ Medium/Problem1457.java
@@ -0,0 +1,39 @@
+package Medium;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Problem1457 {
+    int arr[] = new int[10];
+    int ans=0;
+    public int pseudoPalindromicPaths (TreeNode root) {
+        pseudo(root);
+        return ans;
+    }
+    public void pseudo(TreeNode root){        
+        if(root==null)
+            return;
+        arr[root.val]++;
+        if(root.left==null && root.right==null){
+            int count=0;
+            for(int i=1;i<10;i++)
+                count+=arr[i]%2;
+            if(count==0 || count==1)
+                ans++;
+        }
+        pseudo(root.left);
+        pseudo(root.right);
+        arr[root.val]--;
+    }
+}
