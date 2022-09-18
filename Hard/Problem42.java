e81568b53614a0e99939bd9c0a61198c6d28dfd8
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-09-19 00:47:17 +0530

    Trapping Rain Water

---
+++ Hard/Problem42.java
@@ -0,0 +1,22 @@
+package Hard;
+
+class Problem42 {
+    
+    public int trap(int[] height) {
+        
+        int n = height.length, ans = 0, i;
+        int[] left = new int[n];
+        left[0] = height[0];
+        for(i = 1; i < n; i++)
+            left[i] = Math.max(left[i-1], height[i]);
+        int[] right = new int[n];
+        right[n-1] = height[n-1];
+        for(i = n-2; i >= 0; i--)
+            right[i] = Math.max(right[i+1], height[i]);
+
+        for(i = 0; i < n; i++)
+            ans += Math.max(0, Math.min((left[i]-height[i]), (right[i]-height[i])));
+        
+        return ans;
+    }
+}
