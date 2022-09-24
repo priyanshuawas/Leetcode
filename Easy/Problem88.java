02c4d7d1374cfe0fb6ab1980626575fea9cac8ed
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-09-24 23:43:44 +0530

    Merge Sorted array

---
+++ Easy/Problem88.java
@@ -0,0 +1,11 @@
+package Easy;
+class Problem88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        int counter =0; 
+        for(int i=m;i<m+n;i++){
+            nums1[i] = nums2[counter];
+            counter++;
+        }
+        Arrays.sort(nums1);
+
+    }
