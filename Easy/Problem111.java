9114582b5ca32ee3ee9ad8b41805368f8fb5351f
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-11-03 09:18:27 +0530

    Problem111

---
+++ Easy/Problem111.java
@@ -0,0 +1,18 @@
+package Easy;
+class Problem111 {
+    public int minDepth(TreeNode root) {
+        return helper(root);
+
+        
+    }
+    protected int helper(TreeNode curr){
+        if(curr==null){
+            return 0;
+
+        }
+        int left =helper(curr.left);
+        int right=helper(curr.right);
+        return (left == 0 || right == 0) ? left + right +1: 
+            Math.min(left,right) +1;
+    }
+}
