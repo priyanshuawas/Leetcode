7816c916a59e96ab218aca4a6512146dff127f2e
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-11-03 09:38:50 +0530

    Problem110

---
+++ Easy/Problem110.java
@@ -0,0 +1,26 @@
+package Easy;
+class Problem110 {
+    boolean result= true;
+    public boolean isBalanced(TreeNode root) {
+        validate(root);
+        return result;   
+    }
+    protected  int validate(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int l= validate(root.left);
+        int r= validate(root.right);
+        if(Math.abs(l-r)>1){
+            result = false;
+        }
+        return 1 + Math.max(l, r);
+
+    }
+
+}
+
+
+
+
+
