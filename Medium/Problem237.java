999004c78dd1944ccc79b587f3d3ab7d9ae5845a
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-11-10 09:43:11 +0530

    Problem237

---
+++ Medium/Problem237.java
@@ -0,0 +1,35 @@
+package Medium;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Problem237 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return null;
+        }
+        if(root == p || root == q){
+            return root;
+        }
+        TreeNode left= lowestCommonAncestor(root.left, p,q);
+        TreeNode right= lowestCommonAncestor(root.right,p,q);
+         if(left != null && right != null){
+             return root;
+
+         }
+         if(left != null){
+             return left;
+         }
+         else{
+             return right;
+         }
+        
+        
+    }
+}
