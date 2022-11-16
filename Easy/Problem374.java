ba9d1942c1b88fe5223c90d9673e56006ec60e0b
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-11-16 22:01:27 +0530

    Problem374

---
+++ Easy/Problem374.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class Problem374 extends GuessGame {
+    public int guessNumber(int n) {
+        int start=0,end=n;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(guess(mid)==0)
+                return mid;
+            else if(guess(mid)==-1)
+                end=mid-1;
+            else
+                start=mid+1;
+        }
+        return -1;
+    }
+}
