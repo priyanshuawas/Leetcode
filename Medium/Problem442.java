5a3593a0510f2dfc991ba3d350707d9d23077ede
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-11-25 20:57:46 +0530

    Change

--- Medium/Problem442.java
+++ Medium/Problem442.java
@@ -4,6 +4,7 @@ class Solution {
         char c=0;
         for(int i=0; i<s.length(); ++i){
             c^=s.charAt(i);
+            
         }
         for(int i=0; i<t.length();i++){
             c^=t.charAt(i);
