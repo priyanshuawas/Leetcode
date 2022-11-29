8053a0f04963b82cbc5e750e2c38ec235c73c32c
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-11-29 23:26:58 +0530

    Change

--- Hard/Problem124.java
+++ Hard/Problem124.java
@@ -7,14 +7,11 @@ class Problem124 {
     public int maxPathSum(TreeNode root){
         helper(root);
         return max;
-        
-
     }
     int helper(TreeNode root){
         
         if(root == null){
             return 0;
-
         }
         int left= Math.max(helper(root.left),0);
         int right=Math.max(helper(root.right),0);
