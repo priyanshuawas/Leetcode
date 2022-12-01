cc7f0bbdd869a9c83cf83cc32b244ece794af3fc
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-12-02 00:38:04 +0530

    Problem1504

---
+++ Easy/Problem1504.java
@@ -0,0 +1,19 @@
+package Easy;
+
+class Problem1504 {
+    public boolean halvesAreAlike(String s) {
+        int counts = 0;
+        int half = s.length()/2;
+        for(int i = 0; i<s.length(); i++){
+            if(isVowel(s.charAt(i)))
+                counts = (i<half) ? counts+1 : counts-1;
+        }
+        return (counts==0)?true:false;
+    }
+    public boolean isVowel(char ch){
+        ch = Character.toLowerCase(ch);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        return true;
+        return false;
+    }
+}
