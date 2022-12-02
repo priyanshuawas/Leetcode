b608d65b5304818cfdb56e1b85ba463b41bc9f55
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-12-02 23:19:14 +0530

    Change

--- Easy/Problem58.java
+++ Easy/Problem58.java
@@ -5,11 +5,13 @@ class Problem58 {
         for(int i=s.length()-1;i>=0;i--){
             if(s.charAt(i)!=' '){
                 count++; 
+                
+
             }
             else
                 if(count>0){
                     return count;
-                    
+
                 }
         }
         return count;
