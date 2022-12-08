43120cc686983bdd93c8c7165d803c0e24dcea50
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2022-12-08 21:26:39 +0530

    Problem872

---
+++ Easy/Problem872.java
@@ -0,0 +1,51 @@
+package Easy;
+
+import java.util.ArrayList;
+
+import javax.swing.tree.TreeNode;
+
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) 
+    {
+        ArrayList<Integer> l1=new ArrayList<>();
+        ArrayList<Integer> l2=new ArrayList<>();
+        count(root1,l1);
+        count(root2,l2);
+        if(l1.equals(l2))return true;
+        return false;
+    }
+     public void count(TreeNode n,ArrayList<Integer> l)
+     {
+
+         if(n==null)return;
+         if(n.left==null && n.right==null)
+         {
+            l.add(n.val);
+            return;
+         }
+         count(n.left,l);
+         count(n.right,l);
+     }
+}class Problem872 {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) 
+    {
+        ArrayList<Integer> l1=new ArrayList<>();
+        ArrayList<Integer> l2=new ArrayList<>();
+        count(root1,l1);
+        count(root2,l2);
+        if(l1.equals(l2))return true;
+        return false;
+    }
+     public void count(TreeNode n,ArrayList<Integer> l)
+     {
+
+         if(n==null)return;
+         if(n.left==null && n.right==null)
+         {
+            l.add(n.val);
+            return;
+         }
+         count(n.left,l);
+         count(n.right,l);
+     }
+}
