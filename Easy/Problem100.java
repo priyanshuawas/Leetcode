e68acbbfee2b441b6afd6a1c31a24197e43292e1
priyanshuawas <priyanshu.awas011@gmail.com>
2023-01-10 15:01:13 +0530

    Problem100

---
+++ Easy/Problem100.java
@@ -0,0 +1,19 @@
+package Easy;
+class Problem100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null){
+            return true;
+
+        }
+        if(q == null || p == null) {
+            return false;
+
+        }
+        if(p.val != q.val){
+            return false;
+
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        
+    }
+}
