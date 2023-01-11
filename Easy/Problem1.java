39c0d8ce07be272ea28d56458167e809860bda13
priyanshuawas <priyanshu.awas011@gmail.com>
2023-01-11 23:01:57 +0530

    Problem1

--- Easy/Problem1.java
+++ Easy/Problem1.java
@@ -2,15 +2,19 @@ package Easy;
 
 class Problem1 {
     public int[] twoSum(int[] nums, int target) {
-    
-        for(int i =0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    
-                    return new int[]{i,j};
-                }
+        int[] ans = new int[2];
+        Map map = new HashMap();
+        for(int i=0; i<nums.length;i++){
+            if(!map.containsKey(target - nums[i])){
+                map.put(nums[i],i);
             }
-        }return null;
-        
+            else
+            {
+                ans[1]= i;
+                ans[0]= (int)map.get(target-nums[i]);
+                return ans;
+            }
+        }
+        return ans;
     }
-    }
+}
