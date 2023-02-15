b3a24ec2f36c8927e762c3f1b38733235c196075
priyanshuawas <priyanshu.awas011@gmail.com>
2023-02-15 23:40:08 +0530

    Problem145

---
+++ Easy/Problem145.java
@@ -0,0 +1,37 @@
+package Easy;
+
+import java.util.LinkedList;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class problem145 {
+    LinkedList<Integer> result = new LinkedList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postorder(root);
+        return result;
+    }
+
+    public int postorder(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        result.add(root.val);
+
+        return 0;
+    }
+}
