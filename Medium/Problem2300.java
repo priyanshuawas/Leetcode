3e4b4d411351730034dd9e906ce2a1085839c6f7
priyanshuawas <priyanshu.awas011@gmail.com>
2023-04-03 17:21:03 +0530

    Code 2300

---
+++ Medium/Problem2300.java
@@ -0,0 +1,28 @@
+package Medium;
+
+import java.util.Arrays;
+
+class Problem2300 {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        int m = potions.length;
+        int[] pairs = new int[n];
+        Arrays.sort(potions);
+        for (int i = 0; i < n; i++) {
+            int spell = spells[i];
+            int left = 0;
+            int right = m - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                long pr = (long) spell * potions[mid];
+                if (pr >= success) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            pairs[i] = m - left;
+        }
+        return pairs;
+    }
+}
