943fa5ae8dc940ea82d7f8dd7fbdd5c4c68bde31
priyanshuawas <priyanshu.awas011@gmail.com>
2023-04-04 15:43:08 +0530

    code

--- Easy/Problem141.java
+++ Easy/Problem141.java
@@ -4,6 +4,7 @@ public class Problem141 {
         if(head==null) return false;
         ListNode slow=head;
         ListNode fast=head;
+        
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
