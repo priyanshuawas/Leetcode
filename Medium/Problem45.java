d9bafc7c3c948f574190f0ffde6462c1e35d7a9c
priyanshuawas <priyanshu.awas011@gmail.com>
2023-04-09 20:25:56 +0530

    changes

--- Medium/Problem45.java
+++ Medium/Problem45.java
@@ -9,6 +9,7 @@ class  Problem45 {
         Arrays.fill(dp,Integer.MAX_VALUE);
         dp[n-1]=0;
         
+        
         for(int i=n-2;i>=0;i--){
             int min= Integer.MAX_VALUE;
             for(int j=i+1; j<=Math.min(n-1, i+nums[i]);j++){
--- Medium/Problem75.java
+++ Medium/Problem75.java
@@ -1,25 +1,27 @@
 package Medium;
+
 class Problem75 {
     public void sortColors(int[] nums) {
-    
-        int red=0, y =0,white=0;
-        for(int i =0;i<nums.length;i++){
-            if(nums[i]==0)
+
+        int red = 0, y = 0, white = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
                 red++;
-            if(nums[i]==1)
+            if (nums[i] == 1)
                 white++;
-            y=nums.length-(white+red);
+            y = nums.length - (white + red);
+            
         }
-        for(int i=0;i<red;i++){
-            nums[i]=0;
+        for (int i = 0; i < red; i++) {
+            nums[i] = 0;
         }
-         for(int i=red;i<white+red;i++){
-            nums[i]=1;
-             
+        for (int i = red; i < white + red; i++) {
+            nums[i] = 1;
+
         }
-         for(int i=white+red;i<nums.length;i++){
-            nums[i]=2;
+        for (int i = white + red; i < nums.length; i++) {
+            nums[i] = 2;
         }
-       
-     }
+
     }
+}
