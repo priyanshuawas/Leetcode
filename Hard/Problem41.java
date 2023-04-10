b2069f66bb081b1113260f95fa81d0dd36655a48
priyanshuawas <priyanshu.awas011@gmail.com>
2023-04-10 15:44:33 +0530

    change

--- Hard/Problem41.java
+++ Hard/Problem41.java
@@ -1,7 +1,6 @@
 package Hard;
 public class Problem41 {
     public int firstMissingPositive(int[] nums) {
-
        for (int i = 0; i < nums.length; ) {
             if (nums[i] > 0 && i != nums[i] - 1 && nums[i] -1 < nums.length && nums[nums[i] -1] != nums[i]) {
                 swap(nums, nums[i] - 1, i);
@@ -9,21 +8,15 @@ public class Problem41 {
                 i ++;
             }
         }
-
         for(int i = 0; i < nums.length; i ++) {
             if (nums[i] !=i+1)
                 return i+1;
         }
-
         return nums.length + 1;
     }
-
     public static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
-
         nums[i] = nums[j];
         nums[j] = temp;
     }
-
-
 }
