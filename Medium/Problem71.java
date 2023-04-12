18e6d6dcd9fd6ad2d80cfb60e3922129306c664d
priyanshuawas <priyanshu.awas011@gmail.com>
2023-04-12 15:16:06 +0530

    Code71

---
+++ Medium/Problem71.java
@@ -0,0 +1,25 @@
+package Median;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Problem71 {
+    public String simplifyPath(String path) {
+		Deque<String> stack = new LinkedList<>();
+		for (String item : path.split("/")) {
+			if ("..".equals(item)) {
+				if (!stack.isEmpty()) {
+					stack.pop();
+				}
+			} else if (!item.isEmpty() && !".".equals(item)) {
+				stack.push("/" + item);
+			}
+		}
+		String res = "";
+		while (!stack.isEmpty()) {
+			res = stack.pop() + res;
+		}
+		return res.isEmpty() ? "/" : res;
+	}
+    
+}
