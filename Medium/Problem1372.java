ddd3602fa45fec0ab9f3d8ab35fd09cd3c1e1131
priyanshuawas <priyanshu.awas011@gmail.com>
2023-04-19 14:25:42 +0530

    code

---
+++ Medium/Problem1372.java
@@ -0,0 +1,30 @@
+package Medium;
+class Problem1372 {
+    public int longestZigZag(TreeNode root) {
+        Stack<Node> stack = new Stack<>();
+        stack.add(new Node(root));
+        int max = 0;
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+            if(node.node.left != null){
+                Node leftNode = new Node(node.node.left);
+                leftNode.rightLen = node.leftLen + 1;
+                max = Math.max(max,leftNode.rightLen);
+                stack.push(leftNode);
+            }
+            if(node.node.right != null){
+                Node rightNode = new Node(node.node.right);
+                rightNode.leftLen = node.rightLen + 1;
+                max = Math.max(max,rightNode.leftLen);
+                stack.push(rightNode);
+            }
+        }
+        return max;
+    }
+}
+class Node{
+    int leftLen;
+    int rightLen;
+    TreeNode node;
+    Node(TreeNode node){this.node = node;}
+}
---
+++ node/node/right.java
@@ -0,0 +1,5 @@
+package node.node;
+
+public class right {
+
+}
